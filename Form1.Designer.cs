b6ea1b45f90ac4a05ba0615931f40e9c9dfd89e7
GMERLIN1748 <krittin.i@kkumail.com>
2025-03-09 01:07:38 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Pokemon
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(51, 22);
+            button1.Name = "button1";
+            button1.Size = new Size(174, 50);
+            button1.TabIndex = 0;
+            button1.Text = "Pikachu";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(242, 22);
+            button2.Name = "button2";
+            button2.Size = new Size(174, 50);
+            button2.TabIndex = 1;
+            button2.Text = "Tentacool";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(51, 108);
+            button3.Name = "button3";
+            button3.Size = new Size(174, 50);
+            button3.TabIndex = 2;
+            button3.Text = "Staryu";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(242, 108);
+            button4.Name = "button4";
+            button4.Size = new Size(174, 50);
+            button4.TabIndex = 3;
+            button4.Text = "Goldeen";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(51, 201);
+            button5.Name = "button5";
+            button5.Size = new Size(174, 50);
+            button5.TabIndex = 4;
+            button5.Text = "Ditto";
+            button5.UseVisualStyleBackColor = true;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(242, 201);
+            button6.Name = "button6";
+            button6.Size = new Size(174, 50);
+            button6.TabIndex = 5;
+            button6.Text = "Gardevoir";
+            button6.UseVisualStyleBackColor = true;
+            button6.Click += button6_Click;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(51, 287);
+            button7.Name = "button7";
+            button7.Size = new Size(174, 50);
+            button7.TabIndex = 6;
+            button7.Text = "Wailmer";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(242, 287);
+            button8.Name = "button8";
+            button8.Size = new Size(174, 50);
+            button8.TabIndex = 7;
+            button8.Text = "Arceus";
+            button8.UseVisualStyleBackColor = true;
+            button8.Click += button8_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(474, 22);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(377, 410);
+            pictureBox1.TabIndex = 8;
+            pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1108, 471);
+            Controls.Add(pictureBox1);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ Staryu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pokemon
+{
+    // inheritance from
+    public class Staryu : Pokemon
+    {
+        private ResourceManager Resource1; // Change object to ResourceManager
+        public Staryu()
+        {
+            this.Name = "Staryu";
+            this.Type = PokemonType.Water;
+            this.Type2 = PokemonType.None;
+            this.HP = 30;
+            this.Attack = 45;
+            this.Defense = 55;
+            this.SpecialAttack = 70;
+            this.SpecialDefense = 55;
+            this.Speed = 85;
+            this.Total = 340;
+
+            this.image =
+                Resource1.GetObject("Staryu") as byte[] ?? Array.Empty<byte>();
+        }
+    }
+}
